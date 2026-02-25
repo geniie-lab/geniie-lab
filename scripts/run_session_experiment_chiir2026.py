ec492698d1b4fc36833bf18dc5d39744e1377aa2
Hideo Joho <hideo@slis.tsukuba.ac.jp>
2026-02-25 20:58:35 +0900

    Added run script for chiir2026

---
+++ scripts/run_session_experiment_chiir2026.py
@@ -0,0 +1,98 @@
+# Third-party libraries
+from dotenv import load_dotenv
+import ir_measures
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Local application imports
+from geniie_lab.dataclasses.description import (
+    CorpusDescription,
+    ModelDescription,
+    TaskDescription,
+    ToolDescription,
+    TopicDescription,
+)
+from geniie_lab.dataclasses.topic import (
+    TitleDescriptionNarrativeTopic, FullTopic,
+    TitleDescriptionTopic,
+    TitleNarrativeTopic,
+    TitleOnlyTopic
+)
+from geniie_lab.dataclasses.setting import ExperimentSettings, StageConfig
+from geniie_lab.experiments.session_experiment import ExperimentRunner
+
+load_dotenv()
+
+my_settings = ExperimentSettings(
+    name="my_session_experiment_at_chiir2026",
+    task=TaskDescription(
+        name = "High-Precision Retrieval",
+        description = "Find the most relevant documents at the top rank for a given search topic from a given document collection using a provided search tool.",
+        measurement=[ir_measures.nDCG@10, ir_measures.MRR@10],
+        start_offset=0,
+        serp_size=10
+    ),
+    topicset=TopicDescription(
+        name="beir/scidocs",
+        type="ir_datasets",
+        topic_class=TitleOnlyTopic
+    ),
+    corpus=CorpusDescription(
+        name="SciDocs",
+        description="A document collection of about 260,000 abstracts of academic papers in medical domain, up to around 2020.",
+        index_name="scidocs_bm25",
+    ),
+    models=[
+        ModelDescription(
+            type="openrouter",
+            name="openai/gpt-4o-mini",
+            system_prompt="You're a helpful assistant",
+            temperature=0.0,
+        )
+    ],
+    tools=[
+        ToolDescription(
+            name="opensearch",
+            ranking_model="bm25",
+            index_name="scidocs_bm25",
+            host="localhost",
+            port=9200,
+            use_ssl=False,
+            description="It allows you to perform searches using keywords only and employs the BM25 ranking model to order results.",
+        )
+    ],
+    stages={
+        "query": StageConfig(
+            instruction="""
+                Review the provided descriptions of task, corpus, tool and search topic. Then, formulate a search query.
+            """,
+        ),
+        "ranking": StageConfig(
+            instruction=""
+        ),
+        "click": StageConfig(
+            instruction="""
+                Select a set of documents that are likely to contain relevant information to the search topic. Return an empty list if none of the results appears relevant.
+            """,
+        ),
+        "relevance": StageConfig(
+            instruction="""
+                Evaluate the relevance of the document based on the search topic description and its narrative.
+            """,
+        ),
+        "reformulate": StageConfig(
+            instruction="""
+                Formulate another search query to find new relevant documents.
+            """,
+        ),
+    },
+    plan=["query"],
+    # plan=["query", "ranking"],
+    # plan=["query", "ranking", "click", "relevance", "reformulate", "ranking"],
+    max_topics=1,
+    full_log=False
+)
+
+if __name__ == "__main__":
+    runner = ExperimentRunner(settings=my_settings)
+    runner.run()
